f2a864f4987fdaa2c174ab85449cc585d585f41c
hb5019 <hb5019@hb5019>
2016-12-06 15:16:39 +0900

    2

--- src/main/java/com/hanbit/java/Runner.java
+++ src/main/java/com/hanbit/java/Runner.java
@@ -7,8 +7,10 @@ public class Runner {
 	public static void main(String[] args) {
 		Dog dog = new Dog("도베르만", "검정");
 		Dog dog2 = new Dog("검둥이", "도베르만", "검정");
+
 		
-		System.out.println(dog);
+		
+		System.out.println(dog2);
 		System.out.println(dog.getKind());
 		System.out.println(dog.getColor());
 	}
